932af559107ba2e1003cf54ddb27968cb5bb1765
NamHyungChul <96385615+NamHyungChul@users.noreply.github.com>
2022-07-27 18:17:32 +0900

    Add files via upload

---
+++ ProcessChk/ProcessChk/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessChk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] NetClassClient = Process.GetProcessesByName("iexplore");
+            Console.WriteLine("실행여부확인:" + NetClassClient.Length);
+            System.Threading.Thread.Sleep(5000);
+            if(NetClassClient.Length < 1)
+            {
+                Process.Start(@"C:\Program Files (x86)\Internet Explorer\iexplore.exe");
+            }
+            else
+            {
+                Console.WriteLine("프로그램 정상작동 확인");
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+    }
+}
